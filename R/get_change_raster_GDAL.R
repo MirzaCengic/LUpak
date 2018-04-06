4bd95539a07bb65448eb422e3107a2ae0c7bd927
Mirza Cengic <mirzaceng@gmail.com>
2018-04-06 17:03:37 +0200

    Fixed minor stuff

--- R/get_change_raster_GDAL.R
+++ R/get_change_raster_GDAL.R
@@ -5,11 +5,10 @@
 #'
 #' @param x t1 raster
 #' @param y t2 raster
-#' @param size tilesize
-#' @param outpath Folder in which the temporary raster files will be created. If keep_temp argument is set to FALSE (default), files will be deleted after the process is sucessfully executed. Default path is R temporary directory (see tempdir()).
-#' @param spatial Logical. When TRUE function returns SpatialPointsDataFrame. Default is FALSE.
-#' @param category Land cover category to extract.
+#' @param size Tilesize. If blank, tile size will be \code{1000 * cell resolution}.
+#' @param outpath Folder in which the temporary raster files will be created. Temporary files will be deleted when outfile is created.
 #' @param number_of_cores Number of cores to run. Default is 1.
+#' @param category Land cover category to extract.
 #' @param outfile Output file name. Optional.
 #'
 #' @return None.
@@ -18,16 +17,16 @@
 #' @examples Dunno
 #' @importFrom GSIF getSpatialTiles
 #' @importFrom rgdal GDALinfo readGDAL
-#' @importFrom fs path_temp dir_create
+#' @importFrom fs path_temp dir_create dir_delete
 #' @importFrom raster raster getValues setValues
 #' @importFrom parallel makeCluster stopCluster
 #' @importFrom doParallel registerDoParallel
 #' @importFrom foreach foreach %dopar%
 #' @importFrom gdalR GDAL_mosaic_tile
 #'
-get_change_raster_GDAL <- function(x, y, size, outpath, outfile, values_of_interest, spatial = FALSE, number_of_cores = 2)
+get_change_raster_GDAL <- function(x, y, size, outpath, outfile, category, number_of_cores = 1)
 {
-  stopifnot(!missing(outfile), !missing(values_of_interest))
+  stopifnot(!missing(outfile), !missing(category))
 
   ###########################
   # Get raster path
@@ -53,10 +52,12 @@ get_change_raster_GDAL <- function(x, y, size, outpath, outfile, values_of_inter
   }
 
   fs::dir_create(outpath)
-  print("Par")
 
+  if (number_of_cores > 1)
+  {
+    cat(paste0("Running in parallel with ", number_of_cores, " cores."), "\n")
+  }
 
-  cat(paste0("Running in parallel with ", number_of_cores, " cores."), "\n")
   cl <- parallel::makeCluster(number_of_cores)
   doParallel::registerDoParallel(cl)
   # Define custom function (opposite of %in%)
@@ -82,12 +83,12 @@ get_change_raster_GDAL <- function(x, y, size, outpath, outfile, values_of_inter
     #### Filter out the crop category values from t2 raster
     vals <- raster::getValues(y_ras)
 
-    vals[vals %notin% values_of_interest] <- NA
+    vals[vals %notin% category] <- NA
 
     y_ras <- raster::setValues(y_ras, vals)
     #### Write raster !!CHANGE PATH!!
     diff_raster <- x_ras - y_ras
-    print("vals")
+
     diff_raster_vals <- raster::getValues(diff_raster)
     diff_raster_vals[diff_raster_vals != 0] <- 1
     diff_raster_vals[diff_raster_vals == 0] <- NA
@@ -99,7 +100,7 @@ get_change_raster_GDAL <- function(x, y, size, outpath, outfile, values_of_inter
 
     outmosaic <- paste0(outpath , "/", "tmpmosaic_", i,".tif")
 
-    raster::writeRaster(diff_raster, outmosaic, format = "GTiff", overwrite = TRUE, options = c("COMPRESS=LZW"))
+    raster::writeRaster(diff_raster, outmosaic, format = "GTiff", overwrite = TRUE, options = "COMPRESS=LZW")
   }
   parallel::stopCluster(cl)
   # Delete temporary files if final output file exists
@@ -108,16 +109,20 @@ get_change_raster_GDAL <- function(x, y, size, outpath, outfile, values_of_inter
   out_folder_path <- paste0(outpath, "/*.tif")
 
 
-  # if (file.exists(outfile))
-  # {
-  #   unlink(out_folder_path)
-  # }
 
 
   # Check the bigtiff argument in gdal function
-  x_rcl <- gdalR::GDAL_mosaic_tile(outfile, folder_path = out_folder_path, large_tif = TRUE, return_raster = TRUE)
+  raster_rcl <- gdalR::GDAL_mosaic_tile(outfile, folder_path = out_folder_path, large_tif = TRUE, return_raster = TRUE)
 
-  return(x_rcl)
+  if (file.exists(outfile))
+  {
+    fs::dir_delete(out_folder_path)
+  } else {
+    stop("Error: output file has not been created.")
+  }
+
+
+  return(raster_rcl)
 
 }
 
