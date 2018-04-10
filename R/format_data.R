69bb9c82d0e15c48b26deadec67bccbc93f29204
Mirza Cengic <mirzaceng@gmail.com>
2018-04-10 16:38:32 +0200

    Removed dummy variables creation

--- R/format_data.R
+++ R/format_data.R
@@ -16,6 +16,7 @@
 #' @param VIF_select Do VIF selection of predictiors (only works now for cross_validate = TRUE). Not necessary, since VIF selection is now done a priori.
 #' @param cross_validate Assess models on cross validated or hind casted dataset. Default is FALSE.
 #' @param threshold Thershold for VIF calculation. Not needed anymore (see VIF_select arg).
+#' @param explanatory_variables_eval Raster stack of explanatory variables used for hind casting. Necessary only if cross_validate = FALSE.
 #'
 #' @return List of 2. Training data and evaluation data.
 #' @export
@@ -23,10 +24,9 @@
 #' @examples None.
 #' @importFrom raster extract
 #' @importFrom tidyr drop_na
-#' @importFrom stringr str_detect
-#' @importFrom dummies dummy.data.frame
+#' @importFrom stringr str_detect str_replace
 #' @importFrom caret createDataPartition
-format_data <- function(explanatory_variables, response_variable, evaluation_data,
+format_data <- function(explanatory_variables, response_variable, evaluation_data, explanatory_variables_eval,
                         VIF_select = FALSE, cross_validate = FALSE, threshold = 10)
 {
 
@@ -35,26 +35,28 @@ format_data <- function(explanatory_variables, response_variable, evaluation_dat
     cat(paste0("Performing VIF selection with VIF threshold VIF=", threshold, "..."), "\n")
     explanatory_variables <- vif_select_vars(explanatory_variables, thresh = threshold)
   }
+  if (cross_validate == FALSE & missing(explanatory_variables_eval))
+  {
+    stop("Please add rasters for evaluation if cross_validate = FALSE")
+  }
 
   # Extract values from a raster file for modeling
   region_values_raw <- raster::extract(explanatory_variables, response_variable, sp = TRUE)
   region_values <- region_values_raw@data
 
   # Remove points with NA data
-  region_values_noNA <- region_values %>%
+  region_data_formatted <- region_values %>%
     tidyr::drop_na()
 
   # Make categorical variables as factor
-  categorical_rasters_index <- region_values_noNA %>%
+  categorical_rasters_index <- region_data_formatted %>%
     names() %>%
     stringr::str_detect("catg") %>%
     which()
 
-  region_values_noNA[[categorical_rasters_index[1]]] <- as.factor(region_values_noNA[[categorical_rasters_index[1]]])
-  region_values_noNA[[categorical_rasters_index[2]]] <- as.factor(region_values_noNA[[categorical_rasters_index[2]]])
+  region_data_formatted[[categorical_rasters_index[1]]] <- as.factor(region_data_formatted[[categorical_rasters_index[1]]])
+  region_data_formatted[[categorical_rasters_index[2]]] <- as.factor(region_data_formatted[[categorical_rasters_index[2]]])
 
-  # Make categorical variables as factor
-  region_data_formatted <- dummies::dummy.data.frame(region_values_noNA, sep = "_")
 
   # Prepare data for modeling ####
   # Create data partition - in this case cross validated dataset (66% of the data goes to model training)
@@ -80,29 +82,29 @@ format_data <- function(explanatory_variables, response_variable, evaluation_dat
   } else {
 
     print("hindcast")
-    region_values_evaluation_raw <- raster::extract(explanatory_variables, evaluation_data, sp = TRUE)
+    region_values_evaluation_raw <- raster::extract(explanatory_variables_eval, evaluation_data, sp = TRUE)
     region_evaluation_values <- region_values_evaluation_raw@data
 
     # Remove points with NA data
-    region_values_evaluation_noNA <- region_evaluation_values %>%
+    region_data_evaluation_formatted <- region_evaluation_values %>%
       drop_na()
 
     # Make categorical variables as factor
-    categorical_rasters_index <- region_values_evaluation_noNA %>%
+    categorical_rasters_index <- region_data_evaluation_formatted %>%
       names() %>%
       str_detect("catg") %>%
       which()
 
-    region_values_evaluation_noNA[[categorical_rasters_index[1]]] <- as.factor(region_values_evaluation_noNA[[categorical_rasters_index[1]]])
-    region_values_evaluation_noNA[[categorical_rasters_index[2]]] <- as.factor(region_values_evaluation_noNA[[categorical_rasters_index[2]]])
-
-    # Make categorical variables as factor
-    region_data_evaluation_formatted <- dummies::dummy.data.frame(region_values_evaluation_noNA, sep = "_")
-
+    region_data_evaluation_formatted[[categorical_rasters_index[1]]] <- as.factor(region_data_evaluation_formatted[[categorical_rasters_index[1]]])
+    region_data_evaluation_formatted[[categorical_rasters_index[2]]] <- as.factor(region_data_evaluation_formatted[[categorical_rasters_index[2]]])
 
   }
   # Put hind casting data into empty list
   region_data_for_modeling[["training_data"]] <- region_data_formatted
+  names(region_data_for_modeling[["training_data"]]) <- stringr::str_replace(names(region_data_for_modeling[["training_data"]]), "_fit_catg", "_catg")
+
   region_data_for_modeling[["evaluation_data"]] <- region_data_evaluation_formatted
+  names(region_data_for_modeling[["evaluation_data"]]) <- stringr::str_replace(names(region_data_for_modeling[["evaluation_data"]]), "_eval_catg", "_catg")
+
   return(region_data_for_modeling)
 }
